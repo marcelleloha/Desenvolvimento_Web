6343413b34e5a5531b8ed299775585ab0776d3e6
MARCELLE LOHANE GONCALVES GANIMO <202402726056@alunos.ibmec.edu.br>
2024-09-16 10:58:18 -0300

    subindo versao atualizada da primeira aula

---
+++ aula5/aula.js
@@ -0,0 +1,15 @@
+function func1(x, y){
+    return x + y;
+}
+
+const func2 = function (a, b){
+    return a * b;
+}
+
+const func3 = entrada => entrada + "é bonito demais mesmo"
+
+const minha_lista = [25, "casa", [34, 12], 9.65];
+
+const func4 = elemento => console.log(elemento);
+
+minha_lista.forEach(func4)
