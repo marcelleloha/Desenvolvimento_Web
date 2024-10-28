3384429961d0f41ec3ad7eea26f9bcf97a3d0d03
MARCELLE LOHANE GONCALVES GANIMO <202402726056@alunos.ibmec.edu.br>
2024-10-28 10:04:50 -0300

    subindo aula 7

---
+++ aula7/DOM/arquivo.js
@@ -0,0 +1,5 @@
+const div = document.getElementById("container");
+
+div.querySelector("h1").innerText = "Jucinara";
+div.querySelector("p").innerText = "Jucinara Thaís Soares Paz, mais conhecida como Jucinara (Porto Alegre, 3 de agosto de 1993),é uma jogadora de futebol brasileira que atua como lateral esquerda. Atualmente, joga no Flamengo."
+div.querySelector("img").src = "jucinara.jpg";
